57bf54c62b11516e12cae2dfa7e289529402ba07
Kae Benton <sonja.k.benton@gmail.com>
2020-09-09 02:14:16 -0400

    Created redux store, connected component, moved state to reducer.

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,16 @@
 import React from 'react';
+// import { addFeatures } from '../actions/index';
 
 const AdditionalFeature = props => {
+//  console.log('feature', props.feature)
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" 
+        onClick={() => {props.addFeatures(props.feature)}}
+      
+      >Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
